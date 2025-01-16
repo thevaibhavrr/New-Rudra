7d9a06a95c8ca76821c124b3f72c6c3facdf30c8
vaibhav_rathore <vaibahvrathorema@gmail.com>
2025-01-16 13:05:20 +0530

    logo link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Logosection from '../Components/Home/logosection'
 import Homedetails from '../Components/Home/details'
 import HomeProduct from '../Components/Home/product'
@@ -6,6 +6,15 @@ import Facilities from '../Components/Home/Facilities'
 import Footer from '../Components/footer/footer'
 
 function Home() {
+    const [isInitialLoad, setIsInitialLoad] = useState(true);
+  
+    // Scroll to the top on initial load
+    useEffect(() => {
+      if (isInitialLoad) {
+        window.scrollTo(0, 0); // This scrolls the page to the top
+        setIsInitialLoad(false); // After the first load, set isInitialLoad to false
+      }
+    }, [isInitialLoad]);
   return (
     <div >
         <Logosection/>
